e2ad3bd72be7fe7a160cb4f85fb20244383bfbe1
Cristiano Gotardi <crgotardi@gmail.com>
2021-04-22 21:20:49 -0300

    git for NLW project

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss';
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+
+export function Header() {
+  const date = format (new Date(), 'EEEEEE, d, MMMM', {locale: ptBR})
+  
+  return (
+    <header className={styles.headerContainer}>
+      <img src="#" alt="Podcastr"/>
+      <p>O melhor para você ouvir, sempre</p>
+      <span>{date}</span>
+    </header>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react"
+
+export default function Home(props) {
+  //SPA
+  /*useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  })*/
+
+  return (
+    <h1>Index</h1>
+  )
+}
+
+//SSR
+/*export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+}*/
+
+//SSG
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60*60*24
+  }
+}
